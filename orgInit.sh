756a71e96c845958faaa297f13819d709141aed4
christiantravelling <christian.grant@christiang-ltm8.internal.salesforce.com>
2020-09-28 11:23:38 +0100

    init

--- orgInit.sh
+++ orgInit.sh
@@ -1,8 +1,4 @@
-# Create a scratch org
-sfdx force:org:create -f config/project-scratch-def.json -d 1 -s -a test-deployer
-
-# General user setup niceties - Shane M's plugins
+sfdx shane:org:create -f config/project-scratch-def.json -s -d 1 --userprefix test --userdomain demo.log
+sfdx force:source:push
 sfdx shane:user:password:set -p sfdx1234 -g User -l User
-# sfdx force:data:record:update -s User -w "Name='Integration User'" -v "FirstName='Mary' LastName='Manager'"
-sfdx force:data:record:update -s User -w "Name='User User'" -v "FirstName='Sustainability' LastName='Manager'"
-#
+sfdx force:org:open
