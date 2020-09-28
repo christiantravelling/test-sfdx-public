362e2d01cb8c8e0829c1fd4b8bb905c9d8576c33
christiantravelling <christian.grant@christiang-ltm8.internal.salesforce.com>
2020-09-28 11:31:03 +0100

    init

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,2 @@
-sfdx shane:org:create -f config/project-scratch-def.json -s -d 1 --userprefix test --userdomain demo.log
-sfdx force:source:push
-sfdx shane:user:password:set -p sfdx1234 -g User -l User
+sfdx force:org:create -f config/project-scratch-def.json -s -d 1
 sfdx force:org:open
